b7de7250a7450333a2f39e4b2ce832a7f13d7b9b
Humbert Dominguez <humbertodelac13@gmail.com>
2025-01-15 19:02:28 -0600

    variables de entorno y carpeta publica

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 // importamos express
 const express = require("express");
+require('dotenv').config();
 
 // creamos el server de express (se hace como una función)
 const app = express();
 
 // rutas
 // los callbacks reciben el request y el response
-app.get("/", (req, res) => {
-  return res.json({
-    ok: true,
-    saludo: "saludando ando",
-    nombre: "Humberto de la cruz dominguez",
-    edad: 22,
-  });
-});
+// app.get("/", (req, res) => {
+//   return res.json({
+//     ok: true,
+//     saludo: "saludando ando",
+//     nombre: "Humberto de la cruz dominguez",
+//     edad: 22,
+//   });
+// });
+
+// Directorio publico
+// use es un middleware de express
+app.use( express.static('public') )
 
 // escuchar peticiones, primer arg es el puerto
 // 2do arg es el callback a ejecutar cuando este levantado el server
 app.listen(4000, () => {
   console.log("Servidor corriendo");
+  console.log(process.env.PORT)
 });
