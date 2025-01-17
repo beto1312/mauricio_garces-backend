9ab660a28a170d17b74148930a907e9ce002a3b2
Humbert Dominguez <humbertodelac13@gmail.com>
2025-01-16 18:27:32 -0600

    express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,22 @@
 // ayuda para obtener el intellipsens
 const { response } = require("express");
 
+// obteniendo la funcion que checkea el resultado de la validaciones
+const { validationResult } = require("express-validator");
+
 // los controllers son basicamente las definiciones de
 // los callbacks de los endpoints
 const registerUser = (req, res = response) => {
+  // enviando la request a las reglas de validacion
+  const errors = validationResult(req);
+
+  // si el array de errores no esta vacío
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
   // desestructurando el body de request
   const { name, email, password } = req.body;
 
@@ -17,6 +30,17 @@ const registerUser = (req, res = response) => {
 };
 
 const login = (req, res = response) => {
+  // enviando la request a las reglas de validacion
+  const errors = validationResult(req);
+
+  // si el array de errores no esta vacío
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
   // desestructurando el body de request
   const { email, password } = req.body;
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,36 @@
 // rutas de usuarios auth
 const { Router } = require("express");
 
+// importacion de check de express validator
+// check, checkea que ciertos campos se encuentren en el body de la request
+// con ciertas reglas de validacion
+const { check } = require("express-validator");
+
 const { registerUser, login, renew } = require("../controllers/auth");
 
 // ejecutamos la funcion Router
 const router = Router();
 
 // registro
-router.post("/new", registerUser);
+router.post(
+  "/new",
+  [
+    // que el name sea obligatorio y no este vacio
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El correo es olbigatorio").isEmail(),
+    check("password", "La contrasena es obligatoria").isLength({ min: 5 }),
+  ],
+  registerUser
+);
 
-router.post("/", login);
+router.post(
+  "/",
+  [
+    check("email", "El correo es olbigatorio").isEmail(),
+    check("password", "La contrasena es obligatoria").isLength({ min: 5 }),
+  ],
+  login
+);
 
 router.get("/renew", renew);
 
